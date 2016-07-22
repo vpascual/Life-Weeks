cfff593c7e3294a5d6ab688c55d86c7266f4c2ef
Victor Pascual <victor.pascual@gmail.com>
2016-07-22 14:13:41 +0200

    Added the DataService service

---
+++ app/scripts/services/dataservice.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name lifeWeeksApp.DataService
+ * @description
+ * # DataService
+ * Service in the lifeWeeksApp.
+ */
+angular.module('lifeWeeksApp')
+  .service('DataService', ['$http', function ($http) {
+    return {
+    	getData: function() {
+    		return $http.get('/data/mylife.json');
+    	}
+    }
+  }]);
